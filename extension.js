7f495daa0a28f7f059c6444a0e7b276505c2a199
Claudio J. Nervi P <c.nervi.p@gmail.com>
2012-12-20 15:29:49 -0300

    Added ShowName for configuration

--- extension.js
+++ extension.js
@@ -1,12 +1,16 @@
+const St = imports.gi.St;
 const Panel = imports.ui.main.panel;
 
-let actors, userMenu;
+const ShowName = false;
+
+let icon, actors, userMenu, panelUserMenu;
 
 function init() {
     let statusArea = Panel.statusArea || Panel._statusArea;
     userMenu = statusArea.userMenu; 
+    panelUserMenu = userMenu._iconBox.get_parent();
     actors = [userMenu._iconBox, userMenu._statusChooser.actor, userMenu._notificationsSwitch.actor];
-
+    icon = new St.Icon({icon_name: 'system-shutdown', style_class: 'system-status-icon'});
     let menuItems = userMenu.menu._getMenuItems();
 
     menuItems.forEach(function(menuItem) {
@@ -18,12 +22,22 @@ function init() {
 }
 
 function enable() {
+    if(!ShowName) {
+        userMenu._name.hide()
+        panelUserMenu.insert_child_at_index(icon, -1);
+    }
+
     actors.forEach(function(actor) {
         actor.hide();
     });
 }
 
 function disable() {
+    if(!ShowName) {
+        userMenu._name.show()
+        panelUserMenu.remove_child(icon);
+    }
+
     actors.forEach(function(actor) {
         actor.show();
     });
